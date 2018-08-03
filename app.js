07929350a3990580224d284710f7e719e4bc2e84
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-03 15:11:30 -0400

    Finished the top half of lab

--- app.js
+++ app.js
@@ -70,24 +70,45 @@ p5.sayHello();
 
 //Constructor Function
 
-function PInfo(name, city, age) {
-    this.name = name
-    this.city = city
-    this.age = age
-    PInfo.prototype.greet = function () { 
-        (`My name is ${this.name} I am ${this.age} years old and live in ${this.city}`)
+// function PInfo(name, city, age) {
+//     this.name = name
+//     this.city = city
+//     this.age = age
+//     PInfo.prototype.greet = function () { 
+//         (`My name is ${this.name} I am ${this.age} years old and live in ${this.city}`)
+//     }
+// }
+// //
+// person1 = new PInfo("Nathan", "Norwalk", 22)
+// person2 = new PInfo("Jenna", "Sandusky", 24)
+// person3 = new PInfo("Rachel", "Strongsville", 19)
+// Person4 = new PInfo("Paul", "New London", 22)
+// person1.greet()
+// person2.greet()
+// person3.greet()
+// Person4.greet()
+//ES6 SYNTAX
+class Person {
+    constructor(name, city , age){
+        this.name = name
+        this.city = city
+        this.age = age
+    }
+    greet(){
+        console.log(`My name is ${this.name} I am ${this.age} years old and live in ${this.city}`)
     }
 }
-//
-person1 = new PInfo("Nathan", "Norwalk", 22)
-person2 = new PInfo("Jenna", "Sandusky", 24)
-person3 = new PInfo("Rachel", "Strongsville", 19)
-Person4 = new PInfo("Paul", "New London", 22)
-person1.greet()
-person2.greet()
-person3.greet()
-Person4.greet()
-//ES6 SYNTAX
 
+let person6 = new Person("Nathan", "Norwalk", 22)
+let person7 = new Person("Jenna", "Sandusky", 24)
+let person8 = new Person("Rachel", "Strongsville", 19)
+let person9 = new Person("Rachel", "Strongsville", 19)
+let person10 = new Person("Paul", "New London", 22)
+
+person6.greet();
+person7.greet();
+person8.greet();
+person9.greet();
+person10.greet();
 
 
